91322782fe4dc1a0453b694f8835fd8e08626c96
chia <1335850330@qq.com>
2026-06-15 20:18:20 +0800

    enable auto import

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import ChatView from './views/ChatView.vue';
 </script>
 
 <template>
   <HelloWorld />
-  <HelloWorld></HelloWorld>
+  <ChatView />
 </template>
