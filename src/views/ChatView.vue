dbff98dd159414fcc477eadd1046d78ea9729f5e
chia <1335850330@qq.com>
2026-06-30 17:38:15 +0800

    format

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,45 +1,108 @@
 <template>
-    <div class="container">
-        <div class="bg left">
-            <div class="bg-yellow favicon">111</div>
-            <div class="bg-blue feature">222</div>
-            <div class="bg-green user">333</div>
-        </div>
-        <div class="right">
-            <div class="bg-yellow header">222</div>
-            <div class="bg-blue chat">333</div>
-        </div>
+  <div class="container">
+    <div class="bg left">
+      <div class="bg-red favicon">111</div>
+      <div class="sidebar">
+        <div class="bg-yellow item">4</div>
+        <div class="bg-red item">5</div>
+        <div class="bg-green item">6</div>
+      </div>
+      <div class="bg-green user">333</div>
     </div>
+    <div class="right">
+      <div class="bg-yellow header">
+        <div class="model bg-red">1</div>
+        <div class="header-right">
+          <div class="free bg-green">2</div>
+          <div class="share bg-red">3</div>
+          <div class="more">4</div>
+        </div>
+      </div>
+      <div class="bg-blue chat">
+        <div class="bg-red input">111</div>
+        <div class="bg-yellow tip">ChatGPT也可能会犯错。请核查重要信息。</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style scoped>
 .container {
+  display: flex;
+  height: 100%;
+
+  .left {
+    width: 50px;
     display: flex;
-    height: 100%;
+    flex-direction: column;
 
-    .left {
-        width: 50px;
-
-        .favicon {
-            height: 50px
-        }
+    .favicon {
+      height: 60px;
     }
 
-    .right {
+    .sidebar {
+      flex: 1;
+
+      .item {
+        height: 35px;
+      }
+    }
+
+    .user {
+      height: 70px;
+    }
+  }
+
+  .right {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+
+    .header {
+      height: 55px;
+      display: flex;
+
+      .model {
+        width: 155px;
+      }
+
+      .header-right {
         flex: 1;
         display: flex;
-        flex-direction: column;
+        justify-content: end;
 
-        .header {
-            height: 50px;
+        .free {
+          width: 95px;
         }
 
-        .chat {
-            flex: 1
+        .share {
+          width: 75px;
         }
+
+        .more {
+          width: 55px;
+        }
+      }
     }
+
+    .chat {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: end;
+
+      .input {
+        height: 55px;
+      }
+
+      .tip {
+        display: flex;
+        justify-content: center;
+        height: 35px;
+      }
+    }
+  }
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
@@ -8,9 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
     Components({
-      resolvers: [
-        PrimeVueResolver()
-      ]
-    })
+      resolvers: [PrimeVueResolver()],
+    }),
   ],
-})
+});
