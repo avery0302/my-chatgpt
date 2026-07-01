4da7d0ad3c99baf18cea8c9dbf8796098ee4cf41
chia <1335850330@qq.com>
2026-07-01 20:21:04 +0800

    finish drawing

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,36 +1,90 @@
 <template>
   <div class="container">
-    <div class="bg left">
-      <div class="bd favicon">1</div>
-      <div class="sidebar">
-        <div class="bd item">2</div>
-        <div class="bd item">3</div>
-        <div class="bd item">4</div>
+    <div class="left">
+      <div class="favicon">
+        <img alt="site icon" src="../assets/icon.png" />
       </div>
-      <div class="bd user">
-        <div class="bd round">
-          <i class="pi pi-user"></i>
+      <div class="sidebar">
+        <div class="item">
+          <i class="pi pi-pen-to-square"></i>
+        </div>
+        <div class="item">
+          <i class="pi pi-search"></i>
+        </div>
+        <div class="item">
+          <i class="pi pi-images"></i>
+        </div>
+      </div>
+      <div class="user">
+        <div class="round">
+          <div class="round-border bd">
+            <i class="pi pi-user"></i>
+          </div>
         </div>
       </div>
     </div>
     <div class="right">
-      <div class="bd header">
-        <div class="model bd">5</div>
+      <div class="header">
+        <div class="model">
+          <div class="text">ChatGPT</div>
+          <i class="pi pi-angle-down"></i>
+        </div>
         <div class="header-right">
-          <div class="free bd">6</div>
-          <div class="share bd">7</div>
-          <div class="more">8</div>
+          <div class="free">
+            <i class="pi pi-gift"></i>
+            <div>Free offer</div>
+          </div>
+          <div class="share">
+            <i class="pi pi-upload"></i>
+            <div>Share</div>
+          </div>
+          <div class="more">
+            <i class="pi pi-ellipsis-h"></i>
+          </div>
         </div>
       </div>
-      <div class="bd chat">
-        <div class="bd input">9</div>
-        <div class="bd tip">ChatGPT也可能会犯错。请核查重要信息。</div>
+      <div class="chat">
+        <div class="dev">Under Development</div>
+        <div class="input-area">
+          <div class="input-box">
+            <div class="button-box">
+              <Button
+                iconOnly
+                variant="text"
+                severity="contrast"
+                aria-label="add"
+              >
+                <i class="pi pi-plus"></i>
+              </Button>
+            </div>
+            <InputText placeholder="Ask anything" />
+            <div class="button-box">
+              <Button
+                iconOnly
+                variant="text"
+                severity="contrast"
+                aria-label="voice"
+              >
+                <i class="pi pi-microphone mic"></i>
+              </Button>
+            </div>
+            <div class="button-box">
+              <Button iconOnly severity="contrast" aria-label="send">
+                <i class="pi pi-headphones"></i>
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div class="tip">ChatGPT can make mistakes. Check important info.</div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+const value = ref(null);
+</script>
 
 <style scoped>
 .container {
@@ -38,12 +92,21 @@
   height: 100%;
 
   .left {
-    width: 50px;
+    width: 55px;
     display: flex;
     flex-direction: column;
+    background: rgb(245, 245, 245);
+    border-right: 1px solid rgb(230, 230, 230);
 
     .favicon {
-      height: 55px;
+      height: 60px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      img {
+        height: 30px;
+      }
     }
 
     .sidebar {
@@ -51,6 +114,9 @@
 
       .item {
         height: 35px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
       }
     }
 
@@ -69,8 +135,17 @@
         justify-content: center;
         border-radius: 50%;
 
+        .round-border {
+          border-radius: 50%;
+          height: 13px;
+          width: 13px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+
         i {
-          font-size: 12px;
+          font-size: 9px;
         }
       }
     }
@@ -80,30 +155,67 @@
     flex: 1;
     display: flex;
     flex-direction: column;
+    background: rgb(249, 249, 249);
 
     .header {
+      margin: 0 15px;
       height: 55px;
       display: flex;
+      border-bottom: 1px solid rgb(230, 230, 230);
 
       .model {
-        width: 155px;
+        width: 130px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 4px;
+
+        .text {
+          font-size: 18px;
+          font-weight: 600;
+        }
+
+        i {
+          color: rgb(147, 147, 147);
+        }
       }
 
       .header-right {
         flex: 1;
         display: flex;
         justify-content: end;
+        font-size: 14px;
 
         .free {
-          width: 95px;
+          width: 125px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          gap: 8px;
+          color: rgb(0, 109, 186);
+
+          i {
+            font-size: 16px;
+          }
         }
 
         .share {
           width: 75px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          gap: 8px;
+
+          i {
+            font-size: 15px;
+          }
         }
 
         .more {
-          width: 55px;
+          width: 50px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
         }
       }
     }
@@ -114,15 +226,64 @@
       flex-direction: column;
       justify-content: end;
 
-      .input {
+      .dev {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 60px;
+      }
+
+      .input-area {
         height: 55px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .input-box {
+          width: 65%;
+          height: 54px;
+          border-radius: 27px;
+          border: 1px solid rgb(226, 226, 226);
+          display: flex;
+          background: white;
+
+          input {
+            flex: 1;
+            border: none;
+            padding: 0;
+
+            &::placeholder {
+              color: rgb(150, 150, 150);
+            }
+          }
+
+          .button-box {
+            width: 50px;
+            height: 50px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            button {
+              width: 35px;
+              height: 35px;
+              border-radius: 50%;
+
+              .mic {
+                font-size: 20px;
+              }
+            }
+          }
+        }
       }
 
       .tip {
         display: flex;
+        align-items: center;
         justify-content: center;
         height: 35px;
-        font-size: 12.5px;
+        font-size: 12px;
         color: rgb(157, 157, 157);
       }
     }
